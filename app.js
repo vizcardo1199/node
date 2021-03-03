4963ee531767f0c3c0c03da57811bb09f786e42e
vizcardo1199 <vizcardo1199@gmail.com>
2021-03-03 11:02:25 -0500

    database config

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 
 const express = require('express');
-const mysql = require('mysql');
+const config = require('./config/mysql.config');
+const connection = config.connection;
 const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT;
@@ -9,15 +10,6 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(bodyParser.json());
-
-
-//mysql 
-const connection = mysql.createConnection({
-    host : process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database : process.env.DATABASE_NAME
-});
 
 //route - endpoint
 app.get('/', (req,res)=>
@@ -52,12 +44,49 @@ app.get('/users/:id' , (req,res) =>{
 });
 
 app.post('/add-user',(req,res) =>{
-    res.send('New user')
+    const sql = 'INSERT INTO users SET ?'
+    const usersObj = {
+       us_name: req.body.us_name,
+       us_edad: req.body.us_edad,
+       us_genero: req.body.us_genero,
+       us_pais: req.body.us_pais,
+       us_departamento: req.body.us_departamento,
+       us_distrito: req.body.us_distrito,
+       us_direccion: req.body.us_direccion,
+       us_telefono: req.body.us_telefono,
+       us_mail: req.body.us_mail,
+    }
+    connection.query(sql , usersObj , error => {
+        if(error) throw error ;
+        res.send('User created')    
+    });
 });
 
-app.put('/update-user/:id',(req,res) =>{
-    res.send('Update User')
-});
+
+app.put('/update-user/:id', (req, res) => {
+    const { us_name, us_edad , us_genero , us_pais , us_departamento , us_distrito , us_direccion , us_telefono , us_mail } = req.body;
+    const { row_users } = req.params;
+    const query = `
+      SET @row_users = ?;
+      SET @us_name = ?;
+      SET @us_edad = ?;
+      SET @us_genero = ?;
+      SET @us_pais = ?;
+      SET @us_departamento = ?;
+      SET @us_distrito = ?;
+      SET @us_direccion = ?;
+      SET @us_telefono = ?;
+      SET @us_mail = ?;
+      CALL users(@row_users, @us_name , @us_edad ,@us_genero ,@us_pais ,@us_departamento  , @us_distrito,@us_direccion ,@us_telefono ,SET @us_mail = ?;  );
+    `;
+    connection.query(query, [row_users, us_name, us_edad , us_genero , us_pais , us_departamento , us_distrito , us_direccion , us_telefono , us_mail], (err, rows, fields) => {
+      if(!err) {
+        res.json({status: 'Employee Updated'});
+      } else {
+        console.log(err);
+      }
+    });
+  });
 
 app.delete('/delete-user/:id',(req,res) =>{
     res.send('Delete User')
--- config/mysql.config.js
+++ config/mysql.config.js
@@ -1,19 +1,20 @@
 require('dotenv').config()
-const mysql = require('mysql');
+var mysql = require('mysql');
 
-//mysql 
-const connection = mysql.createConnection({
+config = {
     host : process.env.DATABASE_HOST,
     user: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database : process.env.DATABASE_NAME
+}
+var connection =mysql.createConnection(config); //added the line
+connection.connect(function(err){
+  if (err){
+    console.log('error connecting:' + err.stack);
+  }
+  console.log('connected successfully to DB.');
 });
 
-connection.connect();
- 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
- 
-connection.end();
+module.exports ={
+     connection : mysql.createConnection(config) 
+} 
